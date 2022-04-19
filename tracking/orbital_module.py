02a09787b2817089d43453f8057f76cdb495910c
Jakob <jols19@student.aau.dk>
2022-04-19 12:43:00 +0200

    Auto stash before merge of "main" and "origin/main"

---
+++ tracking/orbital_module.py
@@ -0,0 +1,238 @@
+import numpy as np
+
+#%% Weibel orbit elements to standard format
+def weibeOrbit2radians(weibel_orbit_elem,orbit_time):
+    a = weibel_orbit_elem[0]
+    e = weibel_orbit_elem[1]
+    Omg = weibel_orbit_elem[2]
+    i = weibel_orbit_elem[3]
+    omg = weibel_orbit_elem[4]
+    
+    mu = 398600
+    h = np.sqrt(a*mu*(1-e**2))
+    Omg_rad = Omg*np.pi/180 
+    i_rad = i*np.pi/180
+    omg_rad = omg*np.pi/180
+    theta0 = 270*np.pi/180
+    P = 2*np.pi*np.sqrt((a**3)/mu)
+    theta_dot = 2*np.pi/P * np.sign(90-i)
+    
+    if i==90:
+        theta_dot = 2*np.pi/P
+    
+    theta = theta0 + theta_dot*orbit_time
+    
+    return h,e,Omg_rad,i_rad,omg_rad,theta
+
+#%% Orbital elements to velocity and distance
+def orbitl2velocity(orbital_elements):
+    h,e,Omg_rad,i_rad,omg_rad,theta = orbital_elements
+    
+    mu = 398600
+    rp = np.zeros((len(theta),3))
+    vp = np.zeros((len(theta),3))
+    
+    R3_Omg = np.zeros((len(theta),3,3)) #len(theta) 3x3 matricies
+    R3_omg = np.zeros((len(theta),3,3)) #len(theta) 3x3 matricies
+    R1_i_rad = np.zeros((len(theta),3,3)) #len(theta) 3x3 matricies
+    Q_pX = np.zeros((len(theta),3,3)) #len(theta) 3x3 matricies
+    
+    r = np.zeros((len(theta),3))
+    v = np.zeros((len(theta),3))
+    v_norm = np.zeros(theta.shape)
+    r_norm = np.zeros(theta.shape)
+    
+    for i, thet in enumerate(theta):
+        rp[i] = (h**2/mu) * (1/(1+e*np.cos(thet)))* \
+            (np.cos(thet)*np.array([1,0,0])+np.sin(thet)*np.array([0,1,0]))
+        vp[i] = (mu/h) * (-np.sin(thet)*np.array([1,0,0])+(e+np.cos(thet)) \
+                          *np.array([0,1,0]))
+        
+        R3_Omg[i] = np.array([[np.cos(Omg_rad), np.sin(Omg_rad), 0],
+                                [-np.sin(Omg_rad), np.cos(Omg_rad), 0],
+                                [0, 0, 1]])
+        
+        R1_i_rad[i] = np.array([[1, 0, 0],
+                                [0, np.cos(i_rad), np.sin(i_rad)],
+                                [0, -np.sin(i_rad), np.cos(i_rad)]])
+        
+        R3_omg[i] = np.array([[np.cos(omg_rad), np.sin(omg_rad), 0],
+                              [-np.sin(omg_rad), np.cos(omg_rad), 0],
+                              [0,0,1]])
+        
+        Q_pX[i] = R3_Omg[i].T@R1_i_rad[i].T@R3_omg[i].T
+        
+        r[i] = Q_pX[i]@rp[i]
+        v[i] = Q_pX[i]@vp[i]
+        v_norm[i] = np.linalg.norm(v[i])
+        r_norm[i] = np.linalg.norm(r[i])
+        
+    return r*1000,v*1000,v_norm*1000,r_norm*1000
+
+#%% A a rho rho_dot to velocity and distance
+def R(H, phi, theta):
+    "H is altitude, phi and theta defined the placemement of the radar"
+    R_e = 6378000 # Radius of earth
+    f = 1/(298.257223563) # Earths flattening factor
+    I_hat = np.array([1, 0, 0])  # Unit vector in x direction
+    J_hat = np.array([0, 1, 0])  # Unit vector in y direction
+    K_hat = np.array([0, 0, 1])  # Unit vector in z direction
+    R1 = (R_e/(np.sqrt(1-(2*f-f**2)*np.sin(phi)**2)) + H) * \
+        np.cos(phi)*(np.cos(theta)*I_hat + np.sin(theta)*J_hat)
+    R2 = (H+(R_e*((1-f)**2))/(np.sqrt(1-(2*f-f**2)*(np.sin(phi))**2))) * \
+        np.sin(phi)*K_hat
+    return R1+R2
+
+
+def delta(phi, a, A):
+    "A is azimuth and a is elevation"
+    d = np.arcsin(np.cos(phi)*np.cos(A)*np.cos(a) + np.sin(phi)*np.sin(a))
+    return d
+
+
+def alpha(phi, theta, a, A, delta):
+    if 0 < A < np.pi:
+        h = 2*np.pi - np.arccos((np.cos(phi)*np.sin(a) -
+                                np.sin(phi)*np.cos(A)*np.cos(a))/np.cos(delta))
+        return theta - h
+    if np.pi <= A <= 2*np.pi:
+        h = np.arccos((np.cos(phi)*np.sin(a)-np.sin(phi)
+                      * np.cos(A)*np.cos(a))/np.cos(delta))
+        return theta - h
+
+
+def rho_hat(delta, alpha):
+    I_hat = np.array([1, 0, 0])  # Unit vector in x direction
+    J_hat = np.array([0, 1, 0])  # Unit vector in y direction
+    K_hat = np.array([0, 0, 1])  # Unit vector in z direction
+
+    rho_hat_ = np.cos(alpha)*np.cos(delta)*I_hat+np.sin(alpha)*np.cos(delta)* \
+                J_hat+np.sin(delta)*K_hat
+    return rho_hat_
+
+
+def r(R, distance, rho_hat):
+    "Distance is the range measured from the radar"
+    return R + distance*rho_hat
+
+
+def R_dot(R):
+    K_hat = np.array([0, 0, 1])  # Unit vector i z direction
+    omega_e = 72.92*10**(-6)
+    R_dot = np.cross(omega_e*K_hat, R)
+    return R_dot
+
+
+def delta_dot(A_dot, a_dot, delta, A, a, phi):
+    delta_d = (1/np.cos(delta))*(-A_dot*np.cos(phi)*np.sin(A)*np.cos(a) +
+                                 a_dot*(np.sin(phi)*np.cos(a) - np.cos(phi)*np.cos(A)*np.sin(a)))
+    return delta_d
+
+
+def alpha_dot(A_dot, a_dot, A, a, delta_dot, phi, delta):
+    omega_e = 72.92*10**(-6)
+    a_top = A_dot*np.cos(A)*np.cos(a) - a_dot*np.sin(A) * \
+        np.sin(a) + delta_dot*np.sin(A)*np.cos(a)*np.tan(delta)
+    a_bot = np.cos(phi)*np.sin(a) - np.sin(phi)*np.cos(A)*np.cos(a)
+    alph_dot = a_top/a_bot + omega_e
+    return alph_dot
+
+
+def rho_dot_hat(alpha_dot, alpha, delta, delta_dot):
+    I_hat = np.array([1, 0, 0])  # Unit vector in x direction
+    J_hat = np.array([0, 1, 0])  # Unit vector in y direction
+    K_hat = np.array([0, 0, 1])  # Unit vector i z direction
+    rho_x = (-alpha_dot*np.sin(alpha)*np.cos(delta) -
+             delta_dot*np.cos(alpha)*np.sin(delta))*I_hat
+    rho_y = (alpha_dot*np.cos(alpha)*np.cos(delta) -
+             delta_dot*np.sin(alpha)*np.sin(delta))*J_hat
+    rho_z = delta_dot*np.cos(delta)*K_hat
+    return rho_x + rho_y + rho_z
+
+
+def v(R_dot, rho_dot, rho_hat, rho, rho_dot_hat):
+    v = R_dot + rho_dot*rho_hat + rho*rho_dot_hat
+    return v
+
+def derivative(x,y):
+    Ts = np.diff(x)
+    
+    Dydt = np.diff(y)/Ts
+    xx = x[:-1]+Ts*1/2
+    dydt = np.interp(x,xx,Dydt)
+    
+    return dydt
+
+def round_to_ceil_odd(f):
+  if (np.floor(f)%2 == 0): 
+    return np.floor(f)+1
+  else: 
+    return np.floor(f)
+
+def AaRdR_to_v(A,a,rho,rho_dot,time,placement):
+    '''
+    Parameters
+    ----------
+    A : Azimuth
+    a : elevation
+    rho : Range
+    rho_dot : Radial velocity
+    time 
+    placement : Placement of radar
+
+    Returns
+    -------
+    V : Velocity vector
+    v_mag : Magnityde of V
+    '''
+    placement *= np.pi/180
+    phi,Lambda,H = placement[0],placement[1],placement[2]
+    
+    theta = Lambda
+    
+    A *= np.pi/180
+    
+    A_dot = derivative(time,A)
+    A = A[:-1]
+    
+    a *= np.pi/180
+    a_dot = derivative(time, a)
+    a = a[:-1]
+    
+    rho = rho[:-1]*1000
+    rho_dot = rho_dot[:-1]*1000
+    
+    V = np.zeros((len(A), 3))
+    r_0 = np.zeros((len(A), 3))
+    r_mag = np.zeros(len(A))
+    v_mag = np.zeros(len(A))
+    R_ = R(H, phi, theta)
+    
+    delta_lol = np.zeros(len(A))
+    alpha_lol = np.zeros(len(A))
+    
+    for i in range(len(A)):
+        delta_ = delta(phi, a[i], A[i])
+        delta_lol[i] = delta_
+        alpha_ = alpha(phi, theta, a[i], A[i], delta_)
+        alpha_lol[i] = alpha_
+        
+        rho_hat_ = rho_hat(delta_, alpha_)
+    
+        r_ = r(R_, rho[i], rho_hat_)
+        r_0[i] = r_
+        R_dot_ = R_dot(R_)
+    
+        delta_dot_ = delta_dot(A_dot[i], a_dot[i], delta_, A[i], a[i], phi)
+    
+        alpha_dot_ = alpha_dot(A_dot[i], a_dot[i], A[i],
+                               a[i], delta_dot_, phi, delta_)
+    
+        rho_dot_hat_ = rho_dot_hat(alpha_dot_, alpha_, delta_, delta_dot_)
+    
+        v_ = v(R_dot_, rho_dot[i], rho_hat_, rho[i], rho_dot_hat_)
+        V[i] = v_
+        v_mag[i] = np.linalg.norm(v_)
+        r_mag[i] = np.linalg.norm(r_)
+        
+    return V,v_mag,r_0,r_mag
--- tracking/test.py
+++ tracking/test.py
@@ -1,54 +1,279 @@
-from tracking import velocity_algo
-import matplotlib.pyplot as plt
 import numpy as np
-import scipy.signal
-v, A, t = velocity_algo('truth1.txt', 100, 2, False)
-truth = np.loadtxt('entireOrbit1.txt')
-#truth = scipy.signal.resample(truth, len(t) )
-fake =np.loadtxt('truth1.txt')
-v_true, A_true, t = velocity_algo(truth, 100,2, True)
+import matplotlib.pyplot as plt
+from tracking import import_data
+#from orbital_module import weibeOrbit2radians, orbitl2velocity, AaRdR_to_v
+
+#%% Inputs
+file = "nfft_50k"
+fig_path = "matlab_code_figs"
+sat = 1
+
+#%%
+file_truth = []
+file_false = file+"/false.txt"
+file_entire_orb = []
+for i in range(1,6):
+    file_truth.append(file+"/truth"+f"{i}.txt")
+    file_entire_orb.append(file+"/entireOrbit"+f"{i}.txt")
+
+t_true = []; R_true = []; A_true = []; E_true = []; dR_true = []; SNR_true = []
+
+for i,file_ in enumerate(file_truth):
+    data = import_data(file_)
+    t_true.append(data[0])
+    R_true.append(data[1])
+    A_true.append(data[2])
+    E_true.append(data[3])
+    dR_true.append(data[4])
+    SNR_true.append(data[5])
+    
+t_false,R_false,A_false,E_false,dR_false,SNR_false = import_data(file_false)
+
+t_orb,R_orb,A_orb,E_orb,dR_orb,_ = import_data(file_entire_orb[sat])
+
+
+#%% Test 1
+colors = ["black","blue","orange","cyan","red","magenta"]
+fontsiz = 14
+fig, axs = plt.subplots(2,2, sharex=True,sharey=False,figsize=(14,10))
+fig.subplots_adjust(left=0.1, wspace=0.3)
+#fig.suptitle(f'[$\\beta$, $e$, $\Omega$, $i$, $\omega$] = {weibel_orbit_elem}',fontsize=29)
+
+axs[0][0].plot(t_orb,R_orb,color=colors[sat+1], label=f"Satellite {sat+1}")
+#axs[0][0].scatter(t_true[0],R_true[0], marker='.',color=colors[1],label=f"Satellite 1")
+axs[0][0].set_ylabel("Range [km]",fontsize=fontsiz)
+axs[0][0].grid(True)
+#axs[0][0].yaxis.set_label_coords(x_, y_)
+axs[0][0].set_ylim([np.min(R_true[sat])-2,np.max(R_true[sat])+2])
+
+axs[1][0].plot(t_orb,A_orb,color=colors[sat+1], label=f"Satellite {sat+1}")
+#axs[1][0].scatter(t_true[0],A_true[0], marker='.',color=colors[1],label=f"Satellite 1")
+axs[1][0].set_ylabel("Azimuth [degrees]",fontsize=fontsiz)
+axs[1][0].set_xlabel("Time [s]",fontsize=fontsiz)
+axs[1][0].set_xlim([np.min(t_true[sat])-2,np.max(t_true[sat])+2])
+axs[1][0].grid(True)
+#axs[1][0].yaxis.set_label_coords(x_, y_)
+axs[1][0].set_ylim([np.min(A_true[sat])-2,np.max(A_true[sat])+2])
+
+axs[0][1].plot(t_orb,E_orb,color=colors[sat+1], label=f"Satellite {sat+1}")
+#axs[0][1].scatter(t_true[0],E_true[0], marker='.',color=colors[1],label=f"Satellite 1")
+axs[0][1].set_ylabel("Elevation [degrees]",fontsize=fontsiz)
+axs[0][1].grid(True)
+#axs[0][1].yaxis.set_label_coords(x_, y_)
+axs[0][1].set_ylim([np.min(E_true[sat])-2,np.max(E_true[sat])+2])
+
+axs[1][1].plot(t_orb,dR_orb,color=colors[sat+1], label=f"Satellite {sat+1}")
+#axs[1][1].scatter(t_true[0],dR_true[0], marker='.',color=colors[1],label=f"Satellite 1")
+axs[1][1].set_ylabel("Radial Velocity [km/s]",fontsize=fontsiz)
+axs[1][1].set_xlabel("Time [s]",fontsize=fontsiz)
+axs[1][1].set_xlim([np.min(t_true[sat])-2,np.max(t_true[sat])+2])
+axs[1][1].set_ylim([np.min(dR_true[sat])-2,np.max(dR_true[sat])+2])
+axs[1][1].grid(True)
+#axs[1][1].yaxis.set_label_coords(x_, y_)
+plt.legend(bbox_to_anchor=(1.04,1), loc="upper left", borderaxespad=0,fontsize=fontsiz)
+plt.tight_layout()
+#plt.savefig(fig_path+f"/entire_orb_zoom_{file}.pdf")
 plt.show()
-v_mag = np.zeros(len(v))
-v_mag_true = np.zeros(len(v))
 
-fake[:,0] = fake[:,0] - 10
-plt.plot(fake[:,0],v[:,0])
-plt.plot(truth[:,0], v_true[:,0])
-plt.title('x')
+
+#%% Test 2
+
+fig, axs = plt.subplots(2,2, sharex=True,sharey=False,figsize=(14,10))
+fig.subplots_adjust(left=0.1, wspace=0.3)
+#fig.suptitle(f'[$\\beta$, $e$, $\Omega$, $i$, $\omega$] = {weibel_orbit_elem}',fontsize=29)
+
+
+#axs[0][0].scatter(t_false,R_false, marker='.',color=colors[0],label=f"False alarm")
+axs[0][0].set_ylabel("Range [km]",fontsize=fontsiz)
+axs[0][0].grid(True)
+#axs[0][0].yaxis.set_label_coords(x_, y_)
+#axs[0][0].set_ylim([np.min(R_true[0])-2,np.max(R_true[0])+2])
+
+#axs[1][0].scatter(t_false,A_false, marker='.',color=colors[0],label=f"False alarm")
+axs[1][0].set_ylabel("Azimuth [degrees]",fontsize=fontsiz)
+axs[1][0].set_xlabel("Time [s]",fontsize=fontsiz)
+#axs[1][0].set_xlim([np.min(t_true[0])-2,np.max(t_true[0])+2])
+axs[1][0].grid(True)
+#axs[1][0].yaxis.set_label_coords(x_, y_)
+#axs[1][0].set_ylim([np.min(A_true[0])-2,np.max(A_true[0])+2])
+
+#axs[0][1].scatter(t_false,E_false, marker='.',color=colors[0],label=f"False alarm")
+axs[0][1].set_ylabel("Elevation [degrees]",fontsize=fontsiz)
+axs[0][1].grid(True)
+#axs[0][1].yaxis.set_label_coords(x_, y_)
+#axs[0][1].set_ylim([np.min(E_true[0])-2,np.max(E_true[0])+2])
+
+#axs[1][1].scatter(t_false,dR_false, marker='.',color=colors[0],label=f"False alarm")
+axs[1][1].set_ylabel("Radial Velocity [km/s]",fontsize=fontsiz)
+axs[1][1].set_xlabel("Time [s]",fontsize=fontsiz)
+#axs[1][1].set_xlim([np.min(t_true[0])-2,np.max(t_true[0])+2])
+#axs[1][1].set_ylim([np.min(dR_true[0])-2,np.max(dR_true[0])+2])
+axs[1][1].grid(True)
+#axs[1][1].yaxis.set_label_coords(x_, y_)
+
+for i in range(2):
+    axs[0][0].scatter(t_true[i],R_true[i], marker='.',color=colors[i+1],label=f"Satellite {i+1}")
+    axs[1][0].scatter(t_true[i],A_true[i], marker='.',color=colors[i+1],label=f"Satellite {i+1}")
+    axs[0][1].scatter(t_true[i],E_true[i], marker='.',color=colors[i+1],label=f"Satellite {i+1}")
+    axs[1][1].scatter(t_true[i],dR_true[i], marker='.',color=colors[i+1],label=f"Satellite {i+1}")
+
+plt.legend(bbox_to_anchor=(1.04,1), loc="upper left", borderaxespad=0,fontsize=14)
+plt.tight_layout()
+#plt.savefig(fig_path+f"/sat1_sat2_{file}.pdf")
+plt.show()
+
+#%% Test 3
+
+fig, axs = plt.subplots(2,2, sharex=True,sharey=False,figsize=(14,10))
+fig.subplots_adjust(left=0.1, wspace=0.3)
+#fig.suptitle(f'[$\\beta$, $e$, $\Omega$, $i$, $\omega$] = {weibel_orbit_elem}',fontsize=29)
+
+
+axs[0][0].scatter(t_false,R_false, marker='.',color=colors[0],label=f"False alarm")
+axs[0][0].set_ylabel("Range [km]",fontsize=fontsiz)
+axs[0][0].grid(True)
+#axs[0][0].yaxis.set_label_coords(x_, y_)
+#axs[0][0].set_ylim([np.min(R_true[0])-2,np.max(R_true[0])+2])
+
+axs[1][0].scatter(t_false,A_false, marker='.',color=colors[0],label=f"False alarm")
+axs[1][0].set_ylabel("Azimuth [degrees]",fontsize=fontsiz)
+axs[1][0].set_xlabel("Time [s]",fontsize=fontsiz)
+#axs[1][0].set_xlim([np.min(t_true[0])-2,np.max(t_true[0])+2])
+axs[1][0].grid(True)
+#axs[1][0].yaxis.set_label_coords(x_, y_)
+#axs[1][0].set_ylim([np.min(A_true[0])-2,np.max(A_true[0])+2])
+
+axs[0][1].scatter(t_false,E_false, marker='.',color=colors[0],label=f"False alarm")
+axs[0][1].set_ylabel("Elevation [degrees]",fontsize=fontsiz)
+axs[0][1].grid(True)
+#axs[0][1].yaxis.set_label_coords(x_, y_)
+#axs[0][1].set_ylim([np.min(E_true[0])-2,np.max(E_true[0])+2])
+
+axs[1][1].scatter(t_false,dR_false, marker='.',color=colors[0],label=f"False alarm")
+axs[1][1].set_ylabel("Radial Velocity [km/s]",fontsize=fontsiz)
+axs[1][1].set_xlabel("Time [s]",fontsize=fontsiz)
+#axs[1][1].set_xlim([np.min(t_true[0])-2,np.max(t_true[0])+2])
+#axs[1][1].set_ylim([np.min(dR_true[0])-2,np.max(dR_true[0])+2])
+axs[1][1].grid(True)
+#axs[1][1].yaxis.set_label_coords(x_, y_)
+
+for i in range(2):
+    axs[0][0].scatter(t_true[i],R_true[i], marker='.',color=colors[i+1],label=f"Satellite {i+1}")
+    axs[1][0].scatter(t_true[i],A_true[i], marker='.',color=colors[i+1],label=f"Satellite {i+1}")
+    axs[0][1].scatter(t_true[i],E_true[i], marker='.',color=colors[i+1],label=f"Satellite {i+1}")
+    axs[1][1].scatter(t_true[i],dR_true[i], marker='.',color=colors[i+1],label=f"Satellite {i+1}")
+
+plt.legend(bbox_to_anchor=(1.04,1), loc="upper left", borderaxespad=0,fontsize=14)
+plt.tight_layout()
+plt.savefig(fig_path+f"/sat1_sat2_false_{file}.pdf")
 plt.show()
 
 
-plt.plot(fake[:,0] ,v[:,1])
-plt.plot(truth[:,0], v_true[:,1])
-plt.title('y')
-plt.show()
+#%% Test 4
+t_orb = []; R_orb = []; A_orb = []; E_orb = []; dR_orb = []
+for i,file_ in enumerate(file_entire_orb):
+    data = import_data(file_)
+    t_orb.append(data[0])
+    R_orb.append(data[1])
+    A_orb.append(data[2])
+    E_orb.append(data[3])
+    dR_orb.append(data[4])
+    
+    lower = np.where(t_orb[i]==round(t_true[i][0]))[0][0]
+    upper = np.where(t_orb[i]==round(t_true[i][-1]))[0][0]
+    
+    t_orb[i] = t_orb[i][lower:upper+1]
+    R_orb[i] = R_orb[i][lower:upper+1]
+    A_orb[i] = A_orb[i][lower:upper+1]
+    E_orb[i] = E_orb[i][lower:upper+1]
+    dR_orb[i] = dR_orb[i][lower:upper+1]
+
+fig, axs = plt.subplots(2,2, sharex=True,sharey=False,figsize=(14,10))
+fig.subplots_adjust(left=0.1, wspace=0.3)
+#fig.suptitle(f'[$\\beta$, $e$, $\Omega$, $i$, $\omega$] = {weibel_orbit_elem}',fontsize=29)
 
 
-plt.plot(fake[:,0],v[:,2])
-plt.plot(truth[:,0], v_true[:,2])
-plt.title('z')
+#axs[0][0].scatter(t_false,R_false, marker='.',color=colors[0],label=f"False alarm")
+axs[0][0].set_ylabel("Range [km]",fontsize=fontsiz)
+axs[0][0].grid(True)
+#axs[0][0].yaxis.set_label_coords(x_, y_)
+#axs[0][0].set_ylim([np.min(R_true[0])-2,np.max(R_true[0])+2])
+
+#axs[1][0].scatter(t_false,A_false, marker='.',color=colors[0],label=f"False alarm")
+axs[1][0].set_ylabel("Azimuth [degrees]",fontsize=fontsiz)
+axs[1][0].set_xlabel("Time [s]",fontsize=fontsiz)
+#axs[1][0].set_xlim([np.min(t_true[0])-2,np.max(t_true[0])+2])
+axs[1][0].grid(True)
+#axs[1][0].yaxis.set_label_coords(x_, y_)
+#axs[1][0].set_ylim([np.min(A_true[0])-2,np.max(A_true[0])+2])
+
+#axs[0][1].scatter(t_false,E_false, marker='.',color=colors[0],label=f"False alarm")
+axs[0][1].set_ylabel("Elevation [degrees]",fontsize=fontsiz)
+axs[0][1].grid(True)
+#axs[0][1].yaxis.set_label_coords(x_, y_)
+#axs[0][1].set_ylim([np.min(E_true[0])-2,np.max(E_true[0])+2])
+
+#axs[1][1].scatter(t_false,dR_false, marker='.',color=colors[0],label=f"False alarm")
+axs[1][1].set_ylabel("Radial Velocity [km/s]",fontsize=fontsiz)
+axs[1][1].set_xlabel("Time [s]",fontsize=fontsiz)
+#axs[1][1].set_xlim([np.min(t_true[0])-2,np.max(t_true[0])+2])
+#axs[1][1].set_ylim([np.min(dR_true[0])-2,np.max(dR_true[0])+2])
+axs[1][1].grid(True)
+#axs[1][1].yaxis.set_label_coords(x_, y_)
+
+for i in range(5):
+    axs[0][0].plot(t_orb[i],R_orb[i],color=colors[i+1],label=f"Satellite {i+1}")
+    axs[1][0].plot(t_orb[i],A_orb[i],color=colors[i+1],label=f"Satellite {i+1}")
+    axs[0][1].plot(t_orb[i],E_orb[i],color=colors[i+1],label=f"Satellite {i+1}")
+    axs[1][1].plot(t_orb[i],dR_orb[i],color=colors[i+1],label=f"Satellite {i+1}")
+
+plt.legend(bbox_to_anchor=(1.04,1), loc="upper left", borderaxespad=0,fontsize=14)
+plt.tight_layout()
+plt.savefig(fig_path+f"/multiple_orbits.pdf")
 plt.show()
 
-plt.plot(v_mag)
-plt.plot(v_mag_true)
+#%% Plots 4
+'''
+fig, axs = plt.subplots(2,2, sharex=True,sharey=False,figsize=(14,10))
+fig.subplots_adjust(left=0.1, wspace=0.3)
+#fig.suptitle(f'[$\\beta$, $e$, $\Omega$, $i$, $\omega$] = {weibel_orbit_elem}',fontsize=29)
+x_ = -0.15; y_ = 0.5
+
+axs[0][0].plot(t_orb,R_orb, label="Exact orbit")
+axs[0][0].scatter(t_true[0],R_true[0], marker='.',color=colors[1],label=f"Satellite 1")
+axs[0][0].set_ylabel("Range [km]")
+axs[0][0].grid(True)
+
+axs[1][0].plot(t_orb,A_orb, label="Exact orbit")
+axs[1][0].scatter(t_true[0],A_true[0], marker='.',color=colors[1],label=f"Satellite 1")
+axs[1][0].set_ylabel("Azimuth [degrees]")
+axs[1][0].set_xlabel("Time [s]")
+axs[1][0].grid(True)
+
+axs[0][1].plot(t_orb,E_orb, label="Exact orbit")
+axs[0][1].scatter(t_true[0],E_true[0], marker='.',color=colors[1],label=f"Satellite 1")
+axs[0][1].set_ylabel("Elevation [degrees]")
+axs[0][1].grid(True)
+
+axs[1][1].plot(t_orb,dR_orb, label="Exact orbit")
+axs[1][1].scatter(t_true[0],dR_true[0], marker='.',color=colors[1],label=f"Satellite 1")
+axs[1][1].set_ylabel("Radial Velocity [km/s]")
+axs[1][1].set_xlabel("Time [s]")
+axs[1][1].grid(True)
+plt.legend(bbox_to_anchor=(1.04,1), loc="upper left", borderaxespad=0,fontsize=14)
+plt.tight_layout()
+plt.savefig(fig_path+f"/entire_orb_{file}.pdf")
 plt.show()
 
-plt.plot(fake[:,0], fake[:,1])
-plt.plot(truth[:,0], truth[:,1])
-plt.title("range")
+#%% Plots 2 (SNR)
+plt.scatter(t_false,SNR_false, marker='.',color=colors[0], label="False alarm")
+for i in range(5):
+    plt.scatter(t_true[i],SNR_true[i], marker='.',color=colors[i+1],label=f"Satellite {i+1}")
+plt.grid(True)
+plt.ylabel('SNR')
+plt.xlabel('Time [s]')
+plt.xlim([0,80])
+plt.legend(loc="upper right")
+plt.savefig(fig_path+f'/{file}.pdf')
 plt.show()
-
-plt.plot(fake[:,0],fake[:,2])
-plt.plot(truth[:,0], truth[:,2])
-plt.title("azimtuh")
-plt.show()
-
-plt.plot(fake[:,0],fake[:,3])
-plt.plot(truth[:,0], truth[:,3])
-plt.title("elevation")
-plt.show()
-
-plt.plot(fake[:,0],fake[:,4])
-plt.plot(truth[:,0],truth[:,4])
-plt.title("Radial Velocity")
-plt.show()
+'''
