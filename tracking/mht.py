09b9cdcdf7e3c28b87a3d307887ac7d735f2320c
anders <ahll19@student.aau.dk>
2022-04-26 11:54:14 +0200

    småting

--- tracking/mht.py
+++ tracking/mht.py
@@ -1,93 +1,73 @@
 import numpy as np
 import importlib
 import tracking as tr
+import sys, os
+sys.path.insert(1, os.getcwd())
+
 importlib.reload(tr)
 
-#%% inputs
+# %% inputs
 vmax = 12000
 
-#%% data import and sort
+# %% data import and sort
 
-imports = ["snr50/truth1.txt","snr50/truth2.txt"]
+imports = ["snr50/truth1.txt", "snr50/truth2.txt"]
 
 _data = []
-for i,file_ in enumerate(imports):
+for i, file_ in enumerate(imports):
     _data.append(np.array(tr.import_data(file_)).T)
-    
-    if i == 0:
-        _data[0][:,0] = np.array(_data[0][:,0])-5
 
-data_ = np.concatenate((_data[0],_data[1]))
-data = data_[data_[:,0].argsort()]
+    if i == 0:
+        _data[0][:, 0] = np.array(_data[0][:, 0]) - 5
+
+data_ = np.concatenate((_data[0], _data[1]))
+data = data_[data_[:, 0].argsort()]
 
 data = data[:10]
-#Convert to cartesian coordinates
+# Convert to cartesian coordinates
 time_xyz = tr.conversion(data)
 timesort_xyz = tr.time_slice(time_xyz)
 
-#%% functions
-def init_gate(q1,q2,dt,vmax=12000):
-    if np.linalg.norm(q1-q2) <= vmax*dt:
+
+# %% functions
+def init_gate(q1, q2, dt, vmax=12000):
+    if np.linalg.norm(q1 - q2) <= vmax * dt:
         return True
     else:
         return False
 
+
 def update_inf(individual_hyp):
-    for i in range(len(np.where(np.array(individual_hyp)==-np.inf)[0])):
+    for i in range(len(np.where(np.array(individual_hyp) == -np.inf)[0])):
         pass
+
 
 def pair_hyp(hyp):
     pass
 
+
 def eval_hyp(hyp):
-    
     pass
-    return
 
 
-
-#%% Initial gating
+# %% Initial gating
 
 d_hyp = {}
 hyp_individual = []
 for i in range(len(timesort_xyz[0][0])):
-    init_hyp=[0,i+1]
-    
+    init_hyp = [0, i + 1]
+
     for j in range(len(timesort_xyz[1][0])):
-        if init_gate(timesort_xyz[0][0][i][1:],timesort_xyz[1][0][j][1:],timesort_xyz[1][0][0][0]-timesort_xyz[0][0][0][0]):
+        if init_gate(timesort_xyz[0][0][i][1:], timesort_xyz[1][0][j][1:],
+                     timesort_xyz[1][0][0][0] - timesort_xyz[0][0][0][0]):
             init_hyp.append(-np.inf)
 
     hyp_individual.append(init_hyp)
 
-d_hyp.update({1:np.array(hyp_individual)})
-#hyp = [init_hyp]
+d_hyp.update({1: np.array(hyp_individual)})
+# hyp = [init_hyp]
 for sat in timesort_xyz[2:]:
     number_obs = len(sat[0])
     for sat_i in sat:
-        a=1
-        #print(number_obs, sat_i)
-    
-
-
-'''
-test_names = ["snr50/truth1.txt"]
-
-test_results = tr.velocity_algo(test_names[0])
-
-#%%
-cov_w, cov_u = [np.eye(6)]*2
-x_initial_guess, M_initial_guess = np.ones(6), np.eye(6)
-
-state = test_results[0]
-dt = test_results[1]
-t = test_results[2]
-
-kalman = tr.Kalman(cov_u, cov_w, x_initial_guess, M_initial_guess, dt)
-rvec_time = np.hstack((state[:,:3],np.array([t[:-1]]).T))
-
-init_gate_th = vmax*round(np.diff(rvec_time[:,3][:2])[0],2)
-for i in range(10):
-    
-    kalman.run_sim_mht(state[i])
-    
-'''   
+        a = 1
+        # print(number_obs, sat_i)
