8458c30ee1abe9381e1b3c6cc371cc42c32470f9
Jakob <jols19@student.aau.dk>
2022-04-28 10:47:14 +0200

    prob_update

--- tracking/prob.py
+++ tracking/prob.py
@@ -1,55 +1,102 @@
+import sys, os
+
+sys.path.insert(1, os.getcwd())
+
 import numpy as np
+import importlib
+import tracking as tr
 from itertools import product
+import matplotlib.pyplot as plt
 
 
-def init_gate(q1, q2, dt, vmax=0.5):
+# %% Function definitions
+def init_gate(q1, q2, dt, vmax=12000.0):
     if np.linalg.norm(q1 - q2) <= vmax * dt:
         return True
     else:
         return False
-    
-# simulate data
-m01 = np.random.rand(3)
-m02 = np.random.rand(3)
-m03 = np.random.rand(3)
-S0 = [m01, m02, m03]
-keylist = list(range(1, len(S0)+1))
 
+
+def update_inf(individual_hyp):
+    for i in range(len(np.where(np.array(individual_hyp) == -np.inf)[0])):
+        pass
+
+
+def pair_hyp(hyp):
+    pass
+
+
+def eval_hyp(hyp):
+    pass
+
+
+def create_hyp_table(S0, S1, tracks, initial_hyp=False):
+    # get numbers for new track hypothsis
+    current_tracks = np.sort(list(tracks.keys()))
+    new_track_start = current_tracks[-1] + 1
+    track_numbers = np.arange(new_track_start, new_track_start + len(S1))
+
+    # create initial hypothesis table
+    hyp_table = []
+    for i in range(len(S1)):
+        mn_hyp = [0]
+        for j in range(len(S0)):
+            if init_gate(S0[i], S1[j], 1):
+                mn_hyp.append(j + 1)
+        mn_hyp.append(track_numbers[i])
+        hyp_table.append(mn_hyp)
+
+    # create all possible combinations
+    combinations = [p for p in product(*hyp_table)]
+    perm_table = np.asarray(combinations).T
+
+    # remove impossible combinations
+    non_zero_duplicates = []
+    for i in range(len(perm_table[0])):
+        # if there is a duplicate in column i+1 of perm_table, the value is saved in dup
+        u, c = np.unique(perm_table[:, i], return_counts=True)
+        dup = u[c > 1]
+
+        # if there are non-zero duplicates, non_zero_duplicates gets a True, otherwise it gets a false
+        non_zero_duplicates.append(np.any(dup > 0))
+
+    hyp_possible = np.delete(perm_table, non_zero_duplicates, axis=1)
+
+    return hyp_possible
+
+
+# %% Data import and transformation
+imports = ["snr50/truth1.txt", "snr50/truth2.txt", "nfft_15k/false.txt"]
+
+# slice data
+_data = []
+for i, file_ in enumerate(imports):
+    _data.append(np.array(tr.import_data(file_)).T)
+
+    if i == 0:
+        _data[0][:, 0] = np.array(_data[0][:, 0]) - 5
+    if i == 2:
+        _data[2][:, 0] = np.array(_data[2][:, 0]) + 5
+
+data_ = np.concatenate((_data[0], _data[1]))
+data_ = np.concatenate((data_, _data[2]))
+data = data_[data_[:, 0].argsort()]
+data = data[:12]
+
+# Convert to cartesian coordinates
+time_xyz = tr.conversion(data)
+timesort_xyz = tr.time_slice(time_xyz)
+
+# %% creating initial tracks
+initial_track_keys = list(range(1, timesort_xyz[0].shape[1]+1))
 tracks = {0: []}
-for i in range(len(keylist)):
-    tracks[keylist[i]] = [S0[i]]
+for i in range(len(initial_track_keys)):
+    _key = initial_track_keys[i]
+    _point = timesort_xyz[0][0, i, 1:]
+    tracks[_key] = _point
 
-
-m11 = np.random.rand(3)
-m12 = np.random.rand(3)
-m13 = np.random.rand(3)
-S1 = [m11, m12, m13]
-
-# create initial hypothesis table
-hyp_table = []
-for i in range(len(S1)):
-    mn_hyp = [0]
-    for j in range(len(S0)):
-        if init_gate(S0[i], S1[j], 1, 0.75):
-            mn_hyp.append(j+1)
-    
-    hyp_table.append(mn_hyp)
-
-# create all possible combinations
-combinations = [p for p in product(*hyp_table)]
-perm_table = np.asarray(combinations).T
-
-# remove impossible combinations
-non_zero_duplicates = []
-for i in range(len(perm_table[0])):
-    # if there is a duplicate in column i+1 of perm_table, the value is saved in dup
-    u, c = np.unique(perm_table[:, i], return_counts=True)
-    dup = u[c > 1]
-
-    # if there are non-zero duplicates, non_zero_duplicates gets a True, otherwise it gets a false
-    non_zero_duplicates.append(np.any(dup > 0))
-
-hyp_possible = np.delete(perm_table, non_zero_duplicates, axis=1)
+# %% Initial gating
+hyp1_table = create_hyp_table(timesort_xyz[0][0, :, 1:], timesort_xyz[1][0, :, 1:], tracks, initial_hyp=True)
 
 def N_pdf(mean,Sig_inv):
     Sig = np.linalg.inv(Sig_inv)
@@ -82,7 +129,7 @@ def Pik(H, c=1, P_g=0.2, P_D = 0.2, prior_info=False,
     
     N_TGT = np.max(H)-len(H) #Number of previously known targets
     
-    prob = np.zeros(H.T[0])
+    prob = np.zeros(len(H[0]))
     for i,hyp in enumerate(H.T): 
         N_FT = np.count_nonzero(hyp==0) #Number of false 
         N_NT = np.count_nonzero(hyp>=N_TGT+1) #Number of known targets in hyp
@@ -90,7 +137,7 @@ def Pik(H, c=1, P_g=0.2, P_D = 0.2, prior_info=False,
 
         beta_FT = N_FT/(N_NT+N_DT+N_FT)
         beta_NT = N_NT/(N_FT+N_DT+N_NT)
-        
+
         prob[i] = (1/c) * (P_D**(N_DT) * (1-P_D)**(N_TGT-N_DT) * \
             beta_FT**(N_FT) * beta_NT**(N_NT))
         
@@ -107,6 +154,7 @@ def Pik(H, c=1, P_g=0.2, P_D = 0.2, prior_info=False,
     
     return prob_hyp
 
+
 def prune(prob_hyp,th=0.1,N_h=10000):
     cut_index = np.min(np.where(prob_hyp[0]<th))
     
@@ -116,6 +164,10 @@ def prune(prob_hyp,th=0.1,N_h=10000):
         pruned_hyp = pruned_hyp[:,:N_h]
     
     return pruned_hyp
-    
+
+
+print(Pik(hyp1_table))
+
+
     
 
