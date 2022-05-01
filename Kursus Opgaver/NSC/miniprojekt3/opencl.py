10b39aee768c3af2ad537ab69bddf8a695dcf4f7
Anders <ahll19@student.aau.dk>
2022-05-01 11:44:24 +0200

    miniprojekt done

--- Kursus Opgaver/NSC/miniprojekt3/opencl.py
+++ Kursus Opgaver/NSC/miniprojekt3/opencl.py
@@ -1,22 +1,32 @@
-import time
 import numpy as np
 import pyopencl as cl
-import matplotlib.pyplot as plt
+import unittest
 
 
-def mandelbrot(N, maxiter, pl_id=0, dev_id=0):
+def ravel_plane(N):
     """
-    Calculates the mandelbrot set using opencl
-    :param N: Number of points along the real and imaginary axis to consider
-    :param maxiter: Maximum number of iterations the calculation should consider
-    :param pl_id: platform id index. Defaults to 0, for other platforms check introspection.py from the course
-    :param dev_id: device id index. Defaults to 0, for other devices check introspection.py from the course
-    :return: Returns a NxN matrix where each entry is the number of iterations beofre divergence
+    Specifies the complex plane in terms of a ravelled vector.
+
+    The ravelling was done to simplify the C code of mandelbrot_calc()
+    :param N: The plane is sampled uniformly with N*N points on r(-2, 1) i(-1.5, 1.5)
+    :return: Returns the ravelled vector q, which can be passed to mandelbrot_calc()
     """
     xx = np.arange(-2, 1, 3 / N)
     yy = np.arange(-1.5, 1.5, 3 / N) * 1j
     q = np.ravel(xx + yy[:, np.newaxis]).astype(np.complex64)
 
+    return q
+
+
+def mandelbrot_calc(q, maxiter, pl_id=0, dev_id=0):
+    """
+    Calculates the mandelbrot set using opencl
+    :param q: Array containing the entries of the complex plane. Returned by ravel_plane()
+    :param maxiter: Maximum number of iterations the calculation should consider
+    :param pl_id: platform id index. Defaults to 0, for other platforms check introspection.py from the course
+    :param dev_id: device id index. Defaults to 0, for other devices check introspection.py from the course
+    :return: Returns a NxN matrix where each entry is the number of iterations beofre divergence
+    """
     platform = cl.get_platforms()
     device = [platform[pl_id].get_devices()[dev_id]]
     ctx = cl.Context(devices=device)
@@ -59,4 +69,51 @@ def mandelbrot(N, maxiter, pl_id=0, dev_id=0):
 
     cl.enqueue_copy(queue, output, output_opencl).wait()
 
-    return output.reshape((N, N))
+    n = int(np.sqrt(len(output)))
+
+    return output.reshape((n, n))
+
+
+def mandelbrot(N, maxiter, pl_id=0, dev_id=0):
+    """
+    Calculates the mandelbrot set using opencl
+    :param N: The plane is sampled uniformly with N*N points on r(-2, 1) i(-1.5, 1.5)
+    :param maxiter: Maximum number of iterations the calculation should consider
+    :param pl_id: platform id index. Defaults to 0, for other platforms check introspection.py from the course
+    :param dev_id: device id index. Defaults to 0, for other devices check introspection.py from the course
+    :return: Returns a NxN matrix where each entry is the number of iterations beofre divergence
+    """
+    q = ravel_plane(N)
+    output = mandelbrot_calc(q, maxiter, pl_id=pl_id, dev_id=dev_id)
+
+    return output
+
+
+class TestMandelbrot(unittest.TestCase):
+    """
+    These test casses are based on mathematical statements, which are trivial to prove,
+    which the program should agree with if we are to call it correct.
+    """
+    def test_0_convergence(self):
+        self.res = mandelbrot_calc(np.zeros(1), 20)
+        self.assertEqual(self.res, 0)
+
+    def test_2_divergence(self):
+        self.res = mandelbrot_calc(2*np.ones(1), 20)
+        self.assertNotEqual(self.res, 0)
+
+    def test_no_negatives(self):
+        self.res = mandelbrot(1000, 20)
+        self.nonnegative = self.res >= 0
+        self.result = np.all(self.nonnegative)
+        self.assertTrue(self.result)
+
+
+if __name__ == "__main__":
+    import matplotlib.pyplot as plt
+
+    plt.imshow(mandelbrot(1000, 20))
+    plt.title(r"Mandelbrot: $N=$" + str(1000))
+    plt.show()
+    # uncoment the line below, and run the code in a terminal to unittest the script
+    # unittest.main()
