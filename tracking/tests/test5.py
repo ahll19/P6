abbb1e77c4fc0feb1e3a8275ad4ae1d2bdb776dd
Anders <ahll19@student.aau.dk>
2022-05-18 13:30:10 +0200

    created format guide

--- tracking/tests/test5.py
+++ tracking/tests/test5.py
@@ -38,11 +38,11 @@ def test(snr, nfft):
     all_detections_sliced = tr.time_slice(all_detections[all_detections[:, 0].argsort()])
 
     # initialize the kalman filters
-    mults = [1, 1/50, 1]
+    mults = [1, 1 / 50, 1]
     s_u, s_w, m_init = np.eye(6) * mults[0], np.eye(6) * mults[1], np.eye(6) * mults[2]
-    filter_running = [False]*5
-    x_inits = [td[0, :].reshape(4,) for td in true_detections]
-    x_1s = [td[1, :].reshape(4,) for td in true_detections]
+    filter_running = [False] * 5
+    x_inits = [td[0, :].reshape(4, ) for td in true_detections]
+    x_1s = [td[1, :].reshape(4, ) for td in true_detections]
 
     kalman_filters = [KG(s_u, s_w, x_init, m_init) for x_init in x_inits]
     for x_1, kf in zip(x_1s, kalman_filters):
@@ -86,94 +86,107 @@ def test(snr, nfft):
     return results
 
 
-if __name__ == "__main__":
-    entire_orbit_names = [f"snr50/entireOrbit{i}.txt" for i in range(1, 6)]
-    entire_orbit_dat = []
-    entire_orbit_t = []
+# %%
+entire_orbit_names = [f"snr50/entireOrbit{i}.txt" for i in range(1, 6)]
+entire_orbit_dat = []
+entire_orbit_t = []
 
-    for name in entire_orbit_names:
-        dat, _, t = tr.velocity_algo(name)
-        entire_orbit_dat.append(dat[:, :3])
-        entire_orbit_t.append(t)
-        print(f"Imported {name}")
+for name in entire_orbit_names:
+    dat, _, t = tr.velocity_algo(name)
+    entire_orbit_dat.append(dat[:, :3])
+    entire_orbit_t.append(t)
+    print(f"Imported {name}")
 
-    snrs = [10, 20, 50]
-    nffts = [15, 50]
-    results = []
+# %%
+snrs = [20]
+nffts = [50]
+results = []
 
-    _xyz = ["x", "y", "z"]
+_xyz = ["x", "y", "z"]
+xyz = [r"$r_x\ [m]$", r"$r_y\ [m]$", r"$r_z\ [m]$"]
 
-    for snr in snrs:
-        for nfft in nffts:
-            res = test(snr, nfft)
-            results.append((res, (snr, nfft)))
+for snr in snrs:
+    for nfft in nffts:
+        res = test(snr, nfft)
+        results.append((res, (snr, nfft)))
 
-    for result in results:
-        tracks = result[0]["tracks"]
-        detects = result[0]["true_detections"]
-        all_detects = result[0]["all_detections"]
-        false_detects = result[0]["false_detections"]
-        snr, nfft = result[1]
-        for i in range(3):
-            plt.scatter(all_detects[:, 0], all_detects[:, i+1], marker="x", s=0.2, c='k')
-            plt.ticklabel_format(axis="y", style="sci", scilimits=(0, 0))
-            plt.xlabel("Time [s]")
-            plt.ylabel(xyz[i])
-            plt.title(f"SNR={snr}, NFFT={nfft}k")
-            plt.savefig(f"test5_figs/snr{result[1][0]}_nfft{result[1][1]}_{_xyz[i-1]}_detects.pdf")
-            plt.show()
 
-        for i in range(1, 4):
-            for j in range(5):
-                plt.plot(tracks[j][:, 0], tracks[j][:, i], label="Tracks", c='r', zorder=3, ls='-.')
-                plt.scatter(detects[j][:, 0], detects[j][:, i], label="True detections", s=3, c='b', zorder=2)
-                plt.scatter(false_detects[:, 0], false_detects[:, i], label="False detections", marker="x", s=0.2, c='k', zorder=1)
-                plt.ticklabel_format(axis="y", style="sci", scilimits=(0, 0))
-                plt.xlabel("Time [s]")
-                plt.ylabel(xyz[i-1])
-                plt.title(f"SNR={snr}, NFFT={nfft}")
+# %%
+for result in results:
+    colors = [u'#1f77b4', u'#ff7f0e', u'#2ca02c', u'#d62728',
+              u'#9467bd', u'#8c564b', u'#e377c2', u'#7f7f7f',
+              u'#bcbd22', u'#17becf']
+    ccolors = [u'#e0884b', u'#0080f1', u'#d35fd3', u'#29d8d7',
+               u'#6b9842', u'#73a9b4', u'#1c883d', u'#808080',
+               u'#4342dd', u'#e84130']
+    tracks = result[0]["tracks"]
+    detects = result[0]["true_detections"]
+    all_detects = result[0]["all_detections"]
+    false_detects = result[0]["false_detections"]
+    snr, nfft = result[1]
+    fig1, ax1 = plt.subplots(3, 1, sharex=True, figsize=(14, 10))
+    for i in range(3):
+        ax1[i].scatter(all_detects[:, 0], all_detects[:, i + 1], marker="x", s=1, c='k', alpha=0.7)
+        ax1[i].ticklabel_format(axis="y", style="sci", scilimits=(0, 0))
+        ax1[i].set_ylabel(xyz[i])
 
-            handles, labels = plt.gca().get_legend_handles_labels()
-            by_label = dict(zip(labels, handles))
-            plt.legend(by_label.values(), by_label.keys())
-            plt.savefig(f"test5_figs/snr{result[1][0]}_nfft{result[1][1]}_{_xyz[i-1]}.pdf")
-            plt.show()
+    ax1[-1].set_xlabel("Time [s]")
+    fig1.tight_layout()
+    fig1.savefig(f"test5_figs/snr{result[1][0]}_nfft{result[1][1]}_{_xyz[i - 1]}_detects.pdf")
 
-        print("Saved plots!")
+    fig, ax = plt.subplots(3, 1, sharex=True, figsize=(14, 10))
+    for i in range(1, 4):
+        for j in range(5):
+            ax[i - 1].scatter(false_detects[:, 0], false_detects[:, i], label="False detections", marker="x", s=1,
+                              c='k', zorder=1, alpha=0.7)
+            ax[i - 1].plot(tracks[j][:, 0], tracks[j][:, i], label=f"Track {j + 1}", c=ccolors[j], zorder=3, lw=0.8)
+            ax[i - 1].scatter(detects[j][:, 0], detects[j][:, i], label=f"True detections {j + 1}", s=12, c=colors[j],
+                              zorder=2, alpha=0.5)
+            ax[i - 1].ticklabel_format(axis="y", style="sci", scilimits=(0, 0))
+            ax[i - 1].set_ylabel(xyz[i - 1])
 
-        distances = []
-        for i, track in enumerate(tracks):
-            track_dist = []
-            for j, _t in enumerate(track[:, 0]):
-                _t1, _t2 = np.round(_t, 1), np.round(entire_orbit_t[i], 1)
-                where_res = np.where(_t1 == _t2)[0]
-                if len(where_res) == 1:
-                    idx = int(where_res)
+    ax[-1].set_xlabel("Time [s]")
+    handles, labels = ax[1].get_legend_handles_labels()
+    by_label = dict(zip(labels, handles))
+    fig.legend(by_label.values(), by_label.keys(), bbox_to_anchor=(1.04, 0.8), loc="upper left", borderaxespad=0,
+               fontsize=14)
+    fig.tight_layout()
+    fig.savefig(f"test5_figs/snr{result[1][0]}_nfft{result[1][1]}.pdf", bbox_inches="tight")
 
-                    dx = entire_orbit_dat[i][idx, 0] - track[j, 1]
-                    dy = entire_orbit_dat[i][idx, 1] - track[j, 2]
-                    dz = entire_orbit_dat[i][idx, 2] - track[j, 3]
+    print("Saved plots!")
 
-                    dist = np.sqrt(dx * dx + dy * dy + dz * dz)
-                    track_dist.append(dist)
+    distances = []
+    for i, track in enumerate(tracks):
+        track_dist = []
+        for j, _t in enumerate(track[:, 0]):
+            _t1, _t2 = np.round(_t, 1), np.round(entire_orbit_t[i], 1)
+            where_res = np.where(_t1 == _t2)[0]
+            if len(where_res) == 1:
+                idx = int(where_res)
 
-            distances.append(np.array(track_dist))
+                dx = entire_orbit_dat[i][idx, 0] - track[j, 1]
+                dy = entire_orbit_dat[i][idx, 1] - track[j, 2]
+                dz = entire_orbit_dat[i][idx, 2] - track[j, 3]
 
-        for i, inf in enumerate(zip(tracks, distances)):
-            track, dist = inf[0], inf[1]
-            plt.plot(track[:, 0], dist)
-            plt.xlabel(r"Time $[s]$")
-            plt.ylabel(r"Distance $[m]$")
-            plt.title(f"Track: {i+1}")
-            plt.ticklabel_format(axis="y", style="sci", scilimits=(0, 0))
-            plt.savefig(f"test5_figs/snr{result[1][0]}_nfft{result[1][1]}_dist{i+1}.pdf")
-            plt.show()
-        print("Saved distance plots")
+                dist = np.sqrt(dx * dx + dy * dy + dz * dz)
+                track_dist.append(dist)
 
-        save_names = [f"test5_results/mse_snr{result[1][0]}_nfft{result[1][1]}_track{i}.txt" for i in range(1, 6)]
-        for i, dist in enumerate(distances):
-            mse = np.sum(dist ** 2)/len(dist)
-            with open(save_names[i], "w") as myfile:
-                myfile.write(str(mse))
+        distances.append(np.array(track_dist))
 
-        print("Saved MSEs")
+    for i, inf in enumerate(zip(tracks, distances)):
+        track, dist = inf[0], inf[1]
+        plt.plot(track[:, 0], dist)
+        plt.xlabel(r"Time $[s]$")
+        plt.ylabel(r"Distance $[m]$")
+        plt.ticklabel_format(axis="y", style="sci", scilimits=(0, 0))
+        plt.savefig(f"test5_figs/snr{result[1][0]}_nfft{result[1][1]}_dist{i + 1}.pdf")
+        plt.show()
+    print("Saved distance plots")
+    #
+    # save_names = [f"test5_results/mse_snr{result[1][0]}_nfft{result[1][1]}_track{i}.txt" for i in range(1, 6)]
+    # for i, dist in enumerate(distances):
+    #     mse = np.sum(dist ** 2) / len(dist)
+    #     with open(save_names[i], "w") as myfile:
+    #         myfile.write(str(mse))
+    #
+    # print("Saved MSEs")
