2881a9445f5edf4cea44ba0d456c0317d27a2093
Peter <noergaard2630@hotmail.dk>
2022-05-18 10:12:03 +0200

    mse test5

---
+++ tracking/test5/mseplots_test5.py
@@ -0,0 +1,27 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+snr50NFFT15 = np.load("MSE_snr_50NNFT_15wave_True.npy")[1]
+snr50NFFT50 = np.load("MSE_snr_50NNFT_50wave_True.npy")[1]
+snr20NFFT15 = np.load("MSE_snr_20NNFT_15wave_True.npy")[1]
+snr20NFFT50 = np.load("MSE_snr_20NNFT_50wave_True.npy")[1]
+
+
+data = [snr50NFFT15, snr20NFFT15, snr50NFFT50, snr20NFFT50 ]
+xlabels = ["SNR 50, NNFT 15k","SNR 20, NNFT 15k", "SNR 20, NNFT 50k", "SNR 20, NNFT 50k"]
+
+dim = len(data[0])
+w = 0.75
+dimw = w / dim
+
+fig, ax = plt.subplots()
+x = np.arange(1,len(data)+1)
+for i in range(len(data[0])):
+    y = [d[i] for d in data]
+    b = ax.bar(x + i*dimw, y, dimw, bottom=1*10**3, label = "Sat " + str(i+1))
+
+plt.legend(bbox_to_anchor=(1.04,0.8), loc="upper left", borderaxespad=0,fontsize=14)
+plt.xticks(x+0.25, xlabels, rotation = 20)
+ax.set_yscale('log')
+# ax.set_ylim((10e3, 10e7))
+plt.show()
